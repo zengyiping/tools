8d6e5a40396accba471212bd06c465d317a68745
eping <eping@live.com>
2014-09-27 21:17:29 +0800

    Add git config

---
+++ gitconfig.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+git config --global user.name "eping"
+git config --global user.email "eping@live.com"
+git config --global color.ui true
+git config --global alias.co checkout
+git config --global alias.ci commit
+git config --global alias.st status
+git config --global alias.br branch
